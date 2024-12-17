5e2400b101ab0810ed0eb6061c43f82d3ae642e2
jebutton <jacqueline@jebutton.com>
2024-12-17 12:04:40 -0800

    Created a hack to spit out console commands for testing validity of the datasheet.

--- src/test/context.py
+++ src/test/context.py
@@ -6,3 +6,4 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import data_file_reader
+import data_objects
