96399881eef4631bb291946d698aca510c89e660
jebutton <jacqueline@jebutton.com>
2024-12-19 10:15:46 -0800

    Added pylint block disables to prevent expected pylint errors in context.py

--- src/test/context.py
+++ src/test/context.py
@@ -5,6 +5,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+# pylint: disable=wrong-import-position
+# pylint: disable=import-error
+# pylint: disable=unused-import
 import data_file_reader
 import data_objects
 import menu_views
