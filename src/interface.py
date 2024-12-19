f182d688ab83b48b13e3d0ec528b4acd2e59eb01
jebutton <jacqueline@jebutton.com>
2024-12-19 11:04:27 -0800

    Fixed linebreaks in interface.py

--- src/interface.py
+++ src/interface.py
@@ -12,82 +12,100 @@ def handle_ammo():
     """
         Handles the ammo menu.
     """
+
     ammo_menu = ItemMenu(items_workbook.ammo_data,
                               "Select Ammo Type:")
     menu_result = ammo_menu.display_menu()
     ammo_choice = menu_result[0]
     ammo_amount = menu_result[1]
+
     if ammo_choice != "end":
         print(items_workbook.ammo_data[ammo_choice]
               .get_command(ammo_amount))
+
     return True
 
 def handle_spacesuits():
     """
-        Handles the spacesuit menu
+        Handles the spacesuit menu.
     """
+
     spacesuit_menu = ItemMenu(items_workbook.spacesuit_data,
                               "Select Spacesuit Type:")
     menu_result = spacesuit_menu.display_menu()
     spacesuit_choice = menu_result[0]
     spacesuit_amount = menu_result[1]
+
     if spacesuit_choice != "end":
         print(items_workbook.spacesuit_data[spacesuit_choice]
               .get_command(spacesuit_amount))
+
     return True
 
 def handle_packs():
     """
-        Handles the pack menu
+        Handles the pack menu.
     """
+
     pack_menu = ItemMenu(items_workbook.pack_data,
                               "Select Pack Type:")
     menu_result = pack_menu.display_menu()
     pack_choice = menu_result[0]
     pack_amount = menu_result[1]
+
     if pack_choice != "end":
         print(items_workbook.pack_data[pack_choice]
               .get_command(pack_amount))
+
     return True
 
 def handle_helmets():
     """
-        Handles the helmet menu
+        Handles the helmet menu.
     """
+
     helmet_menu = ItemMenu(items_workbook.helmet_data,
                               "Select Helmet Type:")
     menu_result = helmet_menu.display_menu()
     helmet_choice = menu_result[0]
     helmet_amount = menu_result[1]
+
     if helmet_choice != "end":
         print(items_workbook.helmet_data[helmet_choice]
               .get_command(helmet_amount))
+
     return True
 
 def handle_spacesuit_sets():
     """
-        Handles the spacesuit_set menu
+        Handles the spacesuit_set menu.
     """
+
     spacesuit_set_menu = ItemMenu(items_workbook.spacesuit_set_data,
                               "Select Spacesuit Set Type:")
     menu_result = spacesuit_set_menu.display_menu()
     spacesuit_set_choice = menu_result[0]
     spacesuit_set_amount = menu_result[1]
+
     if spacesuit_set_choice != "end":
         print(items_workbook.spacesuit_set_data[spacesuit_set_choice]
               .get_command(spacesuit_set_amount))
+
     return True
 
 def main():
     """
-        Main loop of program
+        Main loop of program.
     """
+
     exited = False
     not_built_str = "Option Not Built Yet. Try another."
+
     while exited is False:
         main_menu = NavMenu(sorted(items_workbook.sheet_names),
                             "Main Menu:", "Select an option or type quit to exit>")
         menu_selection = main_menu.display_menu().lower()
+
         if menu_selection == "quit":
             exited = True
         elif menu_selection == "ammo":
