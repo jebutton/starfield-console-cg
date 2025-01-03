beefafe6464269d3383311731f22889e858455d6
jebutton <jacqueline@jebutton.com>
2025-01-03 08:21:19 -0800

    Added comments to run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,7 @@
+# This is the script you run via the command line to actually execute the program.
+# It makes sure you have the prerequisites met to run everything in lieu of something
+# like docker.
+
 $virtual_env_path = ".\starfield-console-cg-env"
 $install_python_path = "$($env:LOCALAPPDATA)\Programs\Python\Python312\python.exe"
 
@@ -13,7 +17,7 @@ if (-not(Test-Path -path $virtual_env_path) -Or -not(Test-Path -path $install_py
     Invoke-Expression "$virtual_env_path\Scripts\Activate.ps1"
 }
 
+# Actually run the program.
 Invoke-Expression "$virtual_env_path\Scripts\python.exe -m starfieldccg"
-# &python -m starfieldccg
 
 pause
